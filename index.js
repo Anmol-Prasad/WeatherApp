5a0e4900e9da6e081bd33dcba58960387d5bb7b2
Anmol Prasad <anmol.gpc@gmail.com>
2021-06-17 21:02:28 +0530

    anmol

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const port = process.env.port || 8000;
+
+const templatepath = path.join(__dirname, "WeatherApp/templates/views");
+const partialspath = path.join(__dirname, "WeatherApp/templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", templatepath);
+hbs.registerPartials(partialspath);
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+
+app.get("*", (req, res) => {
+  res.render("error");
+});
+app.listen(port, (err) => {
+  console.log("App working on 8000 💗💗");
+});
