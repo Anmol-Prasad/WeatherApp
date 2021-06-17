5f742af49e8939d2e8fec2cd5770042b9fcac331
Anmol Prasad <anmol.gpc@gmail.com>
2021-06-18 01:02:50 +0530

    anmol

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
-const port = process.env.port || 8000;
+const port = process.env.port || 3000;
 
 const templatepath = path.join(__dirname, "WeatherApp/templates/views");
 const partialspath = path.join(__dirname, "WeatherApp/templates/partials");
